221713cc8713701dec6882be58a7fc9cb6b31c20
Ayush2020 <ayushgiri2021itc@gmail.com>
2025-03-03 18:54:25 +0530

    [Ayush] add . WeekDay

---
+++ SelectionIfElse.js/WeekDay.js
@@ -0,0 +1,16 @@
+let weekDay =  4
+if(weekDay == 1){
+    console.log("Sunday")
+}else if(weekDay == 2){
+    console.log("Monday")
+}else if(weekDay == 3){
+    console.log("Tuesday")
+}else if(weekDay == 4){
+    console.log("Wednesday")
+}else if(weekDay == 5){
+    console.log("Thrusday")
+}else if(weekDay == 6){
+    console.log("Friday")
+}else if(weekDay == 7){
+    console.log("Saturday")
+}
