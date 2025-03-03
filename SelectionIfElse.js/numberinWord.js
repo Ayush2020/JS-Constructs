08729da2a031befa0f29b6ea3671a0f7275a7ae2
Ayush2020 <ayushgiri2021itc@gmail.com>
2025-03-03 18:51:47 +0530

    [Ayush] add . NumberToWord

---
+++ SelectionIfElse.js/numberinWord.js
@@ -0,0 +1,12 @@
+let num =  3
+if(num == 0){
+    console.log("Zero")
+}else if(num == 1){
+    console.log("One")
+}else if(num == 2){
+    console.log("Two")
+}else if(num == 3){
+    console.log("Three")
+}else if(num == 4){
+    console.log("Four")
+}
