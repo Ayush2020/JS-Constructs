cce3e45d96ff5e9e01ef8d7de13296b5ed86ca5f
Ayush2020 <ayushgiri2021itc@gmail.com>
2025-03-03 19:17:22 +0530

    [Ayush] add . NumberToWOrd

---
+++ CaseStatements/numberToWord.js
@@ -0,0 +1,13 @@
+let num = 3
+
+switch(num){
+        case 1 : console.log("One"); break
+        case 2 : console.log("Two"); break
+        case 3 : console.log("Three"); break
+        case 4 : console.log("Four"); break
+        case 5 : console.log("Five"); break
+        case 6 : console.log("Six"); break
+        case 7 : console.log("Seven"); break
+        case 8 : console.log("Eight"); break
+        default : console.log("Invali")
+}
