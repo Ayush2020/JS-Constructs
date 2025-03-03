8b47d768a2624da14056aa579d12317862bc851f
Ayush2020 <ayushgiri2021itc@gmail.com>
2025-03-03 19:19:53 +0530

    [Ayush] add . weekDay

---
+++ CaseStatements/WeekDay.js
@@ -0,0 +1,12 @@
+let weekDay = 3
+
+switch(weekDay){
+        case 1 : console.log("Sunday"); break
+        case 2 : console.log("MOnday"); break
+        case 3 : console.log("Tuesday"); break
+        case 4 : console.log("Wednesday"); break
+        case 5 : console.log("Thrusday"); break
+        case 6 : console.log("Friday"); break
+        case 7 : console.log("Saturday"); break
+        default : console.log("Invalid")
+}
